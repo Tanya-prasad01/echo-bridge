e31e85fbe9956a93c27446666114a2dddc1a9f3b
Tanya Prasad <prasad.tanya01@gmail.com>
2026-08-06 22:33:56 +0530

    Add Hero and Footer components

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return(
+        <nav>
+            <h2>EchoBridge</h2>
+            <a href='/'> Home </a>
+            <a href='/'> Features</a>
+            <a href='/'>About</a>
+            <a href='/'>Contact</a>
+            <button>Start Meeting</button>
+        </nav>
+    );
+}
+
+export default Navbar;
